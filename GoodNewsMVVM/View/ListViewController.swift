e34ab69ff5c4e0447601faab521699941bad8bc4
RJ <r.j.chomik@gmail.com>
2021-03-10 14:11:13 +0000

    added button to the mainvc to dislay new vc containing list of bookmarked quotes

---
+++ GoodNewsMVVM/View/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewController.swift
+//  GoodNewsMVVM
+//
+//  Created by The App Experts on 10/03/2021.
+//  Copyright © 2021 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+//    private var vm:ListViewModel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
