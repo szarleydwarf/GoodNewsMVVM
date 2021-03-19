d1bc11fcad4c6af1e08a8d2dc36a566a2a0c9b81
RJ <r.j.chomik@gmail.com>
2021-03-19 17:28:29 +0000

    testing jenkins with webhook

--- GoodNewsMVVM/Visuals/Toast.swift
+++ GoodNewsMVVM/Visuals/Toast.swift
@@ -15,7 +15,7 @@ class Toast {
         let toast = createSubCenterView(in: view, in: .systemBlue)
         let lab = createSubCenterView(in: toast, in: .systemYellow, with: "SAVED !!!")
         toast.addSubview(lab)
-        self.animations.fadeOut(label: toast, duration: 1.1)
+        self.animations.fadeOut(label: toast, duration: 1.5)
     }
     
     func createSubCenterView(in view: UIView, in color: UIColor, with text:String = "") -> UILabel{
