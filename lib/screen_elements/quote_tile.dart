e7b4ba3db80c9f0e220861f7b0265871e96adab6
Rad <szarleydwarf@>
2023-11-29 20:20:09 +0000

    tile redesign
    added shadow and rounded border

--- lib/screen_elements/quote_tile.dart
+++ lib/screen_elements/quote_tile.dart
@@ -13,19 +13,40 @@ class QuoteTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        quote.author,
-        style: const TextStyle(fontWeight: FontWeight.bold),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 11.0),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 255, 215, 55).withOpacity(0.7),
+            blurRadius: 20.0,
+            offset: const Offset(3.0, 2.0),
+          )
+        ],
       ),
-      trailing: IconButton(
-        onPressed: () {
-          print("DELETING QUOTE");
-        },
-        icon: const Icon(Icons.bookmark_remove_outlined),
-        color: Colors.amber.shade900,
+      child: Card(
+        color: Colors.white,
+        child: ListTile(
+          title: Text(
+            quote.author,
+            style: const TextStyle(fontWeight: FontWeight.w800),
+          ),
+          trailing: IconButton(
+            onPressed: () {
+              print("DELETING QUOTE");
+            },
+            icon: const Icon(Icons.bookmark_remove_outlined),
+            color: Colors.amber.shade900,
+          ),
+          subtitle: Text(quote.quote),
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: Colors.amber.shade900, width: 1),
+            borderRadius: BorderRadius.circular(11),
+          ),
+          tileColor: Colors.amber.shade50,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 11.0),
+        ),
       ),
-      subtitle: Text(quote.quote),
     );
   }
 }
